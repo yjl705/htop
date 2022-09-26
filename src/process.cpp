2470060673929b8ba1ef26f185be54e0a7fecc62
yjl705 <yjl705@163.com>
2022-09-26 13:55:19 -0700

    processor

--- src/process.cpp
+++ src/process.cpp
@@ -5,29 +5,63 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
+
+using namespace LinuxParser;
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// Return this process's ID
+int Process::Pid() { return pid_; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+// Return this process's CPU utilization
+float Process::CpuUtilization() const { 
+  /* get process stats */
+  string line, token;
+  vector<string> procStats;
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+  std::ifstream filestream(kProcDirectory + "/" + to_string(pid_) + kStatFilename);
+  if (filestream.is_open()) {
+    if (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> token) {
+        procStats.push_back(token);
+      }
+    }
+  }
+  
+  /*
+   * Reference: https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat/16736599#16736599
+   * Not a very good safety check, but it protects against reading
+   * empty/invalid files atleast.
+   */
+    if (procStats.size() >= 22) {
+      const long totalTime = std::stol(procStats[13]) +
+                                std::stol(procStats[14]) +
+                                std::stol(procStats[15]) +
+                                std::stol(procStats[16]);
+        return ((float)totalTime/sysconf(_SC_CLK_TCK))/UpTime();
+    } else {
+        return 0;
+    }
+}
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// Return the command that generated this process
+string Process::Command() { return LinuxParser::Command(pid_); }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// Return this process's memory utilization
+string Process::Ram() { return LinuxParser::Ram(pid_); }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// Return the user (name) that generated this process
+string Process::User() { return LinuxParser::User(pid_);}
 
-// TODO: Overload the "less than" comparison operator for Process objects
+// Return the age of this process (in seconds)
+long int Process::UpTime() const { return LinuxParser::UpTime(pid_); }
+
+// Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+bool Process::operator<(Process const& a) const {
+    return (CpuUtilization() - a.CpuUtilization()) > 0;
+}
