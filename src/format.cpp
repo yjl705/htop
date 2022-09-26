b8c9c975ad5e43499e62aa4c048f27422fd727cf
yjl705 <yjl705@163.com>
2022-09-26 14:03:36 -0700

    change time format

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,19 @@
 
 using std::string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {           
+    constexpr long secInMin = 60;
+    constexpr long secInHr = 3600;
+
+    const long hours = seconds/secInHr;
+    seconds = seconds % secInHr;
+    const long minutes = seconds/secInMin;
+    seconds = seconds % secInMin;
+
+    return std::to_string(hours).append(":")
+    .append(std::to_string(minutes)).append(":")
+    .append(std::to_string(seconds));
+}
